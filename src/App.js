c65c7499f919a733a931d69cdc70262008d4531a
kiwoon kim <kisoon0947@gmail.com>
2022-04-30 16:43:09 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from './components/ExpenseItem'
+import React from 'react'
 
-function App() {
+import Expenses from './components/Expenses/Expenses'
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -25,11 +27,8 @@ function App() {
 
   return (
     <>
-      <h1>React</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+      <h2>REACT</h2>
+      <Expenses items={expenses} />
     </>
   )
 }
